8cfc9cfb2367ad1f87f1147d7541a4a78cbac954
nelius72 <cornelioromeroborrero@gmail.com>
2026-02-05 18:58:33 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: ['image.tmdb.org'],
-  },
-};
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',      // opcional, deja vacío si no hay puerto
+        pathname: '/t/p/**'  // permite todos los paths bajo /t/p/
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
